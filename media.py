e5ded7f90a5d409335018db49106fbbef6709766
Mohd Ejaz Siddiqui <mohdejazsiddiqui@gmail.com>
2015-08-26 23:45:27 +0530

    added docstring to the class

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 #class for movies.
 class Movies():
     def __init__(self, title, images, url, lead_actors):
+        """ Class with different attributes of a Movie """
         self.title = title
         self.poster_image_url = images
         self.trailer_youtube_url = url
